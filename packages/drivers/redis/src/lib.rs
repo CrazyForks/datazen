1a86fe45340ab5c712d741525873651ae5ff85ae
flyxl <wuxiaolongklws@gmail.com>
2026-09-14 09:59:35 +0800

    feat(redis): restore redis_driver as split modules + KEEPTTL/EXPIREAT methods

--- packages/drivers/redis/src/lib.rs
+++ packages/drivers/redis/src/lib.rs
@@ -14,7 +14,10 @@ mod ops_json;
 mod ops_observe;
 mod ops_pubsub;
 mod ops_stream;
+mod redis_value;
+mod redis_driver_on;
 mod redis_driver;
+mod redis_driver_traits;
 pub use connect::{build_connection_plan, ConnectionPlan, RedisLiveConn, TlsPlan, Topology};
 pub use ops::{set_settings_allow_flush, settings_allow_flush};
 
